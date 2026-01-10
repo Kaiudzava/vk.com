fc31e8196e58342fca9f5b6ae808f2441c11704d
Kaiudzava <130813333+Kaiudzava@users.noreply.github.com>
2026-01-11 04:59:39 +0500

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) { ... }
+module.exports = function handler(req, res) { ... }
 POST /api/send
 if (req.method !== 'POST') {
   return res.status(405).json({ ok: false });
