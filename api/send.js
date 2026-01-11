7217d24c167a752635072c69147c813095c76579
Kaiudzava <130813333+Kaiudzava@users.noreply.github.com>
2026-01-11 17:30:47 +0500

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,5 +1,8 @@
 // api/send.js
 
+const TG_TOKEN = "8552207692:AAHTZxlESsKrDzgrS5XYl4GF_OWYlw6OY70";
+const TG_CHAT_ID = -1003528330262;
+
 module.exports = async function handler(req, res) {
   // Разрешаем только POST
   if (req.method !== 'POST') {
@@ -16,16 +19,22 @@ module.exports = async function handler(req, res) {
     // Учебный лог — просто показать, ЧТО ПРИШЛО
     console.log('Получены :', body);
 
-    /**
-     * Пример того, что может прийти:
-     * {
-     *   contact: "test@mail.com"
-     * }
-     * или
-     * {
-     *   wish: "123456"
-     * }
-     */
+    // 👇 ДОБАВИЛИ: отправка в Telegram
+    const message = `
+📩 НОВАЯ ЗАЯВКА
+
+Данные:
+${JSON.stringify(body, null, 2)}
+    `;
+
+    await fetch(`https://api.telegram.org/bot${TG_TOKEN}/sendMessage`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        chat_id: TG_CHAT_ID,
+        text: message,
+      }),
+    });
 
     return res.status(200).json({
       ok: true,
